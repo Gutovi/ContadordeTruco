cad31868ecaaac84d078e54f1554378012f0239b
Gutovi <gutovi.kun@gmail.com>
2018-05-26 19:10:31 -0300

    Half-way through the options.

---
+++ app/src/main/java/gutovi/contadordetruco/OpcionesDelJuego.java
@@ -0,0 +1,13 @@
+package gutovi.contadordetruco;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpcionesDelJuego extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opciones_del_juego);
+    }
+}
