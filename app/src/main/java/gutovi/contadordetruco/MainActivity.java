c48c7feee4134526ba83def4ea46ec599d1ce2fc
Gutovi <gutovi.kun@gmail.com>
2018-05-27 03:43:15 -0300

    Most design done.

--- app/src/main/java/gutovi/contadordetruco/MainActivity.java
+++ app/src/main/java/gutovi/contadordetruco/MainActivity.java
@@ -2,9 +2,16 @@ package gutovi.contadordetruco;
 
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Space;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,14 +21,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     FloatingActionButton fabNuevoJuego = findViewById(R.id.fabAgregarJuego);
+    TextView lblNoHayJuegos = findViewById(R.id.lblNoHayJuego);
+    Space spcBottom = findViewById(R.id.spcBottomSpace);
 
-    fabNuevoJuego.setOnClickListener(new View.OnClickListener() {
+
+    TextView lblNombres1 = findViewById(R.id.lblJug1);
+    TextView lblNombres2 = findViewById(R.id.lblJug2);
+    TextView lblPtos1 = findViewById(R.id.lblPuntos1);
+    TextView lblPtos2 = findViewById(R.id.lblPuntos2);
+
+    Button btnEnvido = findViewById(R.id.btnEnvido);
+    Button btnRealEnvido = findViewById(R.id.btnRealEnvido);
+    Button btnFaltaEnvido = findViewById(R.id.btnFaltaEnvido);
+
+
+    if (getIntent().getExtras() != null){
+        lblNoHayJuegos.setVisibility(View.GONE);
+        spcBottom.setVisibility(View.GONE);
+        fabNuevoJuego.setRotation(45);
+
+    }else{
+        lblNoHayJuegos.setVisibility(View.VISIBLE);
+        spcBottom.setVisibility(View.VISIBLE);
+        fabNuevoJuego.setRotation(0);
+    }
+
+        fabNuevoJuego.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent abrirOpciones = new Intent (MainActivity.this, OpcionesDelJuego.class );
+            Intent abrirOpciones = new Intent (MainActivity.this, OpcionesDelJuego.class);
             startActivity ( abrirOpciones );
         }
     });
-    }
 
+    }
 }
--- app/src/main/java/gutovi/contadordetruco/OpcionesDelJuego.java
+++ app/src/main/java/gutovi/contadordetruco/OpcionesDelJuego.java
@@ -1,11 +1,14 @@
 package gutovi.contadordetruco;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import java.io.Console;
@@ -19,7 +22,13 @@ public class OpcionesDelJuego extends AppCompatActivity {
     TextView lblJugadores;
     String numJugadores;
     Integer prgValor;
+    Integer NumeroChicos;
+    Switch swtFlor;
 
+    EditText Chicos;
+
+    EditText Eq1Jug1;
+    EditText Eq2Jug1;
     EditText Eq1Jug2;
     EditText Eq2Jug2;
     EditText Eq1Jug3;
@@ -30,16 +39,23 @@ public class OpcionesDelJuego extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opciones_del_juego);
 
+        Button BotonAceptar = findViewById(R.id.btnCrearJuego);
 
         prgJugadores =  findViewById(R.id.prgNumJugadores);
         lblJugadores = findViewById(R.id.lblNumJugadores);
         prgValor = prgJugadores.getProgress();
         numJugadores = Integer.toString((prgValor+1)*2);
 
+        Chicos = findViewById(R.id.txtNumeroChicos);
+
+        Eq1Jug1 = findViewById(R.id.txtEq1Jug1);
+        Eq2Jug1 = findViewById(R.id.txtEq2Jug1);
         Eq1Jug2 = findViewById(R.id.txtEq1Jug2);
         Eq2Jug2 = findViewById(R.id.txtEq2Jug2);
         Eq1Jug3 = findViewById(R.id.txtEq1Jug3);
         Eq2Jug3 = findViewById(R.id.txtEq2Jug3);
+
+        swtFlor = findViewById(R.id.swtFlor);
 
         prgJugadores.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -78,6 +94,29 @@ public class OpcionesDelJuego extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        BotonAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                NumeroChicos = Integer.parseInt(Chicos.getText().toString());
+
+                Intent iniciarPartida = new Intent(OpcionesDelJuego.this, MainActivity.class);
+                iniciarPartida.putExtra("NumeroJugadores",Integer.parseInt(numJugadores));
+                iniciarPartida.putExtra("Flor",swtFlor.isChecked());
+                iniciarPartida.putExtra("NumeroChicos",NumeroChicos);
+
+                iniciarPartida.putExtra("E1J1",Eq1Jug1.getText());
+                iniciarPartida.putExtra("E1J1",Eq1Jug2.getText());
+                iniciarPartida.putExtra("E1J1",Eq1Jug3.getText());
+                iniciarPartida.putExtra("E1J1",Eq2Jug1.getText());
+                iniciarPartida.putExtra("E1J1",Eq2Jug2.getText());
+                iniciarPartida.putExtra("E1J1",Eq2Jug3.getText());
+
+                startActivity(iniciarPartida);
 
             }
         });
